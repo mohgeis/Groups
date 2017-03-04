1fca24afa1516f9392911e4a51cdd6e4595479de
mohgeis <mohgeis@gmail.com>
2017-03-04 20:38:54 +0100

    updates

--- README.md
+++ README.md
@@ -1,2 +1,34 @@
 # Groups
-Create teams from groups with maximum diversity in teams
+Create mixed teams from already existing groups (list from lists)
+
+Example:
+
+Group 1:
+ a1,b1,c1
+
+Group 2:
+ a2,b2,c2,d2
+
+Group 3:
+ a3,b3
+
+Group 4:
+ a4,b4,c4
+
+Gruop 5:
+ a5,b5,c5,d5,e5
+
+- Teams of 3:
+  * ['a3', 'c1', 'b4']
+  * ['c5', 'b2', 'a4']
+  * ['a5', 'a2', 'a1']
+  * ['e5', 'd2', 'b1']
+  * ['d5', 'c2', 'c4']
+  * ['b3', 'b5']
+
+- Teams of 4:
+  * ['a3', 'b1', 'b4', 'a2']
+  * ['e5', 'b2', 'a4', 'a1']
+  * ['d5', 'd2', 'c4', 'c1']
+  * ['a5', 'b3', 'c5', 'c2']
+  * ['b5']
--- main.py
+++ main.py
@@ -2,16 +2,17 @@ from FileHandler import read_file
 from time import sleep
 
 if __name__ == "__main__":
-    all_groups = read_file("Fika_list.txt")
+    all_groups = read_file("list.txt")
     print (all_groups)
+    team_size = 4
 
     while True:
-        team = all_groups.get_team(3)
+        team = all_groups.get_team(team_size)
         all_groups.sort_groups()
         if len(team) == 0:
             break
-        if len(team) < 3:
+        if len(team) < team_size:
             print (team)
             break
         print(team)
-        sleep (0.5)
+        sleep (0.3)
